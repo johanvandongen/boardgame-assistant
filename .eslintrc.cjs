ba290dc2fdf6d9a8374c9dddd5b2a70982eaa62c
johanvandongen <74316747+johanvandongen@users.noreply.github.com>
2024-06-12 10:32:22 +0200

    add prettier error highlighting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,8 +9,9 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "prettier"],
   rules: {
+    "prettier/prettier": "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
