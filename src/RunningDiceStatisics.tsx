7076f7299733e4ca32289754725162e580289747
johanvandongen <74316747+johanvandongen@users.noreply.github.com>
2024-07-01 10:29:31 +0200

    add slider

--- src/RunningDiceStatisics.tsx
+++ src/RunningDiceStatisics.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { useState } from "react";
 import { NumberInputButtonGrid } from "./NumberInputButtonGrid";
 import { Rolls } from "./Rolls";
+import { Slider } from "@mui/material";
 
 export interface IRunningDiceStatisTicsProps {}
 
@@ -14,6 +15,7 @@ export function RunningDiceStatisTics() {
     //     10, 9, 10, 7, 2, 7, 5, 7, 6, 8, 9, 5, 8, 4, 9, 11, 8, 6, 10,
     // ];
     const [data, setData] = useState<number[]>([]);
+    const [sliderValue, setSliderValue] = useState<number[]>([1, 1]);
 
     const addNumber = (num: number) => {
         if (num === -1) {
@@ -23,13 +25,47 @@ export function RunningDiceStatisTics() {
         }
     };
 
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        setSliderValue(newValue as number[]);
+    };
+
+    React.useEffect(() => {
+        // let newMax = sliderValue[1];
+        // if (sliderValue[1] === data.length - 1) {
+        //     newMax = data.length;
+        // }
+        // setSliderValue((prev) => [prev[0], newMax]);
+        setSliderValue((prev) =>
+            sliderValue[1] >= data.length - 1 ? [prev[0], Math.max(1, data.length)] : prev
+        );
+    }, [data]);
+
     return (
         <RunningDiceContinaer>
-            <div>slider</div>
+            <SliderContainer>
+                <p>
+                    Dice distribution over rolls: {sliderValue[0]} to {sliderValue[1]}
+                </p>
+                <Slider
+                    getAriaLabel={() => "Temperature range"}
+                    value={sliderValue}
+                    min={1}
+                    max={data.length}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    getAriaValueText={(value: number) => value.toString()}
+                    marks={[
+                        { value: 1, label: 1 },
+                        { value: sliderValue[0], label: sliderValue[0] },
+                        { value: sliderValue[1], label: sliderValue[1] },
+                        { value: data.length, label: data.length },
+                    ]}
+                />
+            </SliderContainer>
             <VisualisationContainer>
-                <Histogram values={data} />
+                <Histogram values={data.slice(sliderValue[0] - 1, sliderValue[1])} />
             </VisualisationContainer>
-            <Rolls data={data} />
+            <Rolls data={data.slice(sliderValue[0] - 1, sliderValue[1])} />
             <NumberInputButtonGrid addNumber={addNumber} />
         </RunningDiceContinaer>
     );
@@ -42,8 +78,14 @@ const RunningDiceContinaer = styled.div`
     gap: 2rem;
 `;
 
+const SliderContainer = styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: var(--light-gray);
+`;
+
 const VisualisationContainer = styled.div`
-    /* border: 1px solid green; */
-    width: 100%;
-    /* height: 100vh; */
+    width: 90%;
 `;
