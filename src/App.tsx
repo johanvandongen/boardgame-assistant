266fd438db9592fe0ece3101fd8bfae17f0eb57c
johanvandongen <74316747+johanvandongen@users.noreply.github.com>
2024-07-02 08:42:24 +0200

    decompose themecontext

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {
     AppBar,
     Autocomplete,
     Box,
-    CssBaseline,
     IconButton,
     InputBase,
     Toolbar,
@@ -15,14 +14,13 @@ import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import { EmptyPage } from "./EmptyPage";
 import Catan from "./catan/Catan";
-import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import React from "react";
+import { ColorModeContext } from "./ThemeContext";
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
-
-function App() {
+export default function App() {
     type GameItem = {
         label: string;
         component: JSX.Element | null;
@@ -161,36 +159,3 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
         },
     },
 }));
-
-export default function ToggleColorMode() {
-    const [mode, setMode] = React.useState<"light" | "dark">("light");
-    const colorMode = React.useMemo(
-        () => ({
-            toggleColorMode: () => {
-                setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-            },
-        }),
-        []
-    );
-
-    const theme = React.useMemo(
-        () =>
-            createTheme({
-                palette: {
-                    mode,
-                },
-            }),
-        [mode]
-    );
-
-    return (
-        <ColorModeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <App />
-            </ThemeProvider>
-        </ColorModeContext.Provider>
-    );
-}
-
-// export default App;
